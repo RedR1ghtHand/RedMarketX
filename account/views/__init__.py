0550eb66f1418087621a13fe0adc7ee0000b584d
RedR1ghtHand <4aikaman@gmail.com>
2025-06-27 18:29:56 +0300

    feature(account) add __all__ list inside views/__init__.py

--- account/views/__init__.py
+++ account/views/__init__.py
@@ -1,3 +1,5 @@
 from .auth import LoginView
 from .register import RegisterView
 from .settings import SettingsView, ChangePasswordView
+
+__all__ = ['LoginView', 'RegisterView', 'SettingsView', 'ChangePasswordView']
